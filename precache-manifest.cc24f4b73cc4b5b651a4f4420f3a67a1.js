d7e7203b531bd049d6afcceee5e66f217dd0b56c
uneet7 <uneet.patel@thecarousell.com>
2022-05-28 21:37:03 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.cc24f4b73cc4b5b651a4f4420f3a67a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c50bd4e4def3bddcaa8c64ab8a681d7",
+    "url": "/personal-blog/index.html"
+  },
+  {
+    "revision": "9c08666975a6551ffaad",
+    "url": "/personal-blog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ed47c81eb6fa1d420259",
+    "url": "/personal-blog/static/js/2.06db84a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-blog/static/js/2.06db84a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c08666975a6551ffaad",
+    "url": "/personal-blog/static/js/main.f457fa01.chunk.js"
+  },
+  {
+    "revision": "fca4d7109cbae466979d",
+    "url": "/personal-blog/static/js/runtime-main.2b08fe6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
